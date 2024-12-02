70ad4da9c60a2f4cdfdad4b04c9bb8631a529eab
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-12-02 14:26:04 -0300

    .

--- PaginaPrincipal/paginaPrincipal.js
+++ PaginaPrincipal/paginaPrincipal.js
@@ -2,12 +2,12 @@ function mostrarCarta(personagem) {
     const cartas = document.querySelectorAll('.carta');
     cartas.forEach(carta => {
         carta.classList.remove('visivel');
-        carta.style.display = 'none'; // Garante que todas as cartas sejam ocultas
+        carta.style.display = 'none';
     });
 
     const cartaSelecionada = document.getElementById(`carta-${personagem}`);
     if (cartaSelecionada) {
-        cartaSelecionada.style.display = 'block'; // Exibe a carta
-        setTimeout(() => cartaSelecionada.classList.add('visivel'), 10); // Aplica a animação
+        cartaSelecionada.style.display = 'block';
+        setTimeout(() => cartaSelecionada.classList.add('visivel'), 10);
     }
 }
